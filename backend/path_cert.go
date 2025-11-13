44615df83f1c8738d270fc4398442c5642f1cf14
Matthew Endsley <mendsley@gmail.com>
2025-11-13 01:49:53 -0800

    Add debug logging for dns-01 options

--- backend/path_cert.go
+++ backend/path_cert.go
@@ -106,14 +106,18 @@ func (b *backend) certsRead(ctx context.Context, req *logical.Request, data *fra
 		return nil, errwrap.Wrapf("failed to create DNS provider: {{err}}", err)
 	}
 
+	b.Logger().Debug("Using dns challenge provider", "provider", provider)
+
 	var opts []dns01.ChallengeOption
 
 	if len(b.dnsResolvers) > 0 {
 		opts = append(opts, dns01.AddRecursiveNameservers(dns01.ParseNameservers(b.dnsResolvers)))
+		b.Logger().Debug("Setting DNS nameservers to", "dns-resolvers", b.dnsResolvers)
 	}
 
 	if b.skipAuthoritativeNSCheck || provider == "nil" {
 		opts = append(opts, dns01.DisableAuthoritativeNssPropagationRequirement())
+		b.Logger().Debug("Skipping authoritative NS checks")
 	}
 	if err := client.Challenge.SetDNS01Provider(dnsProvider, opts...); err != nil {
 		return nil, err
